98f6fe053729d0f30a423430dd7897addb3be80d
M1Y0 <yogurtcumirac@gmail.com>
2026-04-25 21:19:04 +0300

    ilk uygulama yüklendi

---
+++ File-metods.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace hafta5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool devammi = true;
+            while (devammi == true)
+        { 
+            string DosyaAdi;
+
+            Console.WriteLine("Menüyü Kapatmak için 0'a basınız");
+            Console.WriteLine("1-Dosya Oluşturma/Dosyaya Ekleme Yapma");
+            Console.WriteLine("2-Dosya Okuma");
+            Console.WriteLine("3-Dosya Silme");
+            Console.WriteLine("4-Dosya Yazma(StreamWriter)");
+            Console.WriteLine("5-Dosya Okuma(StreamReader)");
+
+
+
+            Console.WriteLine("Yapmak istediğiniz işlemi seçiniz");
+            int islem = Convert.ToInt32(Console.ReadLine());
+                if (islem==0)
+                {
+                    devammi = false;
+                    Console.WriteLine("Program Kapatılıyor...");
+                    
+
+                }
+                else
+                    {
+                    Console.WriteLine("Dosya Adını Giriniz");
+                    DosyaAdi = Console.ReadLine();
+                    DosyaAdi += ".txt";
+
+                    switch (islem)
+                    {
+
+                        case 1:
+                            #region Dosya Oluşturma
+
+                            if (!File.Exists(DosyaAdi))
+                            {
+
+                                File.WriteAllText(DosyaAdi, $"{DosyaAdi} Dosyası:");
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Bu isimde bir dosya zaten var.");
+                                Console.WriteLine("Dosyaya ekleme yapmak ister misiniz? (evet/hayır)");
+
+                                if (Console.ReadLine() == "evet")
+                                {
+                                    Console.WriteLine("Eklencek şeyleri yazınız");
+                                    string eklencek = Console.ReadLine();
+                                    eklencek = "\n" + eklencek;
+                                    File.AppendAllText(DosyaAdi, eklencek);
+                                }
+                            }
+                            #endregion
+                            break;
+                        case 2:
+                            #region Dosya Okuma
+                            string dosyaIcerigi;
+                            List<string> dosyadakiSatirlar = File.ReadLines(DosyaAdi).ToList();
+                            for (int i = 0; i < dosyadakiSatirlar.Count; i++)
+                            {
+                                Console.WriteLine(dosyadakiSatirlar[i]);
+                                Console.WriteLine("---------------------------------------");
+                            }
+                            #endregion
+                            break;
+                        case 3:
+                            #region Dosya Silme
+                            Console.WriteLine("Dosya silinecek, onaylıyor musunuz? (evet/hayır)");
+                            string silinsinMi = Console.ReadLine();
+
+                            if ((File.Exists(DosyaAdi)) && (silinsinMi == "evet"))
+                            {
+                                File.Delete(DosyaAdi);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Dosya Yok");
+                            }
+                                #endregion
+                                break;
+                        case 4:
+                            #region Dosya Yazma(sw)
+                            using (StreamWriter swYazici = new StreamWriter(DosyaAdi, true)) //true varsa var olan dosyadakiler silinmeden üstüne yazılır
+                            {
+                                Console.WriteLine("Metin Gir");
+                                string metin = Console.ReadLine();
+                                swYazici.WriteLine(metin);
+                            }
+                            #endregion
+                            break;
+                        case 5:
+                            #region Dosya Okuma(sr)
+                            if (File.Exists(DosyaAdi))
+                            {
+                                using (StreamReader srOku = new StreamReader(DosyaAdi))
+                                {
+                                    string yazi;
+                                    Console.WriteLine("--------------------");
+                                    while ((yazi = srOku.ReadLine()) != null)
+                                    {
+
+                                        Console.WriteLine($"{yazi}");
+                                    }
+                                }
+                            }
+                            else
+                            {
+
+                                Console.WriteLine("Dosya Yok");
+                            }
+                            #endregion
+                            break;
+                    }
+                  }
+        }
+              
+
+        }
+    }
+}
